dad93449ca370f045bd597c00f5d31ee0da18000
jsh901220 <joe1220@daum.net>
2019-06-20 18:51:36 +0900

    [env: styled-components] - styled components types, ThemeProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { createBrowserHistory } from 'history'
 import { Provider, observer } from 'mobx-react'
 import { Switch, Router, Route } from 'react-router-dom'
 import { RouterStore, syncHistoryWithStore } from 'mobx-react-router'
+import { ThemeProvider } from 'styled-components'
 
 import rootStore from 'stores/RootStore'
 import { Home } from 'containers'
+import styles from 'themes/styles'
 
 const browserHistory = createBrowserHistory()
 const history = syncHistoryWithStore(browserHistory, rootStore.routing)
@@ -19,11 +21,13 @@ class App extends React.Component<{}> {
   render() {
     return (
       <Provider {...RouterStore}>
-        <Router history={history}>
-          <Switch>
-            <Route path='/' component={Home} />
-          </Switch>
-        </Router>
+        <ThemeProvider theme={styles}>
+          <Router history={history}>
+            <Switch>
+              <Route path='/' component={Home} />
+            </Switch>
+          </Router>
+        </ThemeProvider>
       </Provider>
     )
   }
