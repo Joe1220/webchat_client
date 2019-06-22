54deec54f08599f624a610296b0250b4129f168c
jsh901220 <joe1220@daum.net>
2019-06-22 21:36:16 +0900

    [env: storybook] - styled components theme 추가

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,19 @@
-import { configure } from '@storybook/react';
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import { ThemeProvider } from 'styled-components'
+import theme from '../src/components/_settings/styles'
+
 // automatically import all files ending in *.stories.tsx
-const req = require.context('../src', true, /\.stories\.tsx$/);
+const req = require.context('../src', true, /\.stories\.tsx$/)
 
 function loadStories() {
   req.keys().forEach(req);
 }
 
-configure(loadStories, module);
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    {story()}
+  </ThemeProvider>
+))
+
+configure(loadStories, module)
