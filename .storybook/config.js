eb4e541b4486a2c728072bcb266b7a5c37c694fb
jsh901220 <joe1220@daum.net>
2019-07-03 19:54:07 +0900

    [dev: storybook] - [fix] router 설정 추가

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { configure, addDecorator } from '@storybook/react'
 import { ThemeProvider } from 'styled-components'
+import styled from 'styled-components'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+
 import theme from '../src/components/_settings/styles'
+
+const StyledContainer = styled('div')`
+  padding: 10px;
+`
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../src', true, /\.stories\.tsx$/)
@@ -10,10 +18,16 @@ function loadStories() {
   req.keys().forEach(req);
 }
 
-addDecorator((story) => (
-  <ThemeProvider theme={theme}>
-    {story()}
-  </ThemeProvider>
-))
+addDecorator((story) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Router history={createBrowserHistory()}>
+        <StyledContainer> 
+          {story()}
+        </StyledContainer>
+      </Router>
+    </ThemeProvider>
+  )
+})
 
 configure(loadStories, module)
