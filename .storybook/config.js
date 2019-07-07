c9e0f6ee1c809f159370983cf0d7a99133e8d756
jsh901220 <joe1220@daum.net>
2019-07-07 17:40:11 +0900

    [env: storybook] - [fix] knobs 애드온 추가, config 파일에 global 스타일, router 추가

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Provider } from 'mobx-react'
 import { configure, addDecorator } from '@storybook/react'
 import { ThemeProvider } from 'styled-components'
 import styled from 'styled-components'
 import { Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+import { withKnobs } from '@storybook/addon-knobs'
 
+import rootStore from '../src/stores/RootStore'
 import theme from '../src/components/_settings/styles'
 
 const StyledContainer = styled('div')`
@@ -20,14 +23,18 @@ function loadStories() {
 
 addDecorator((story) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Router history={createBrowserHistory()}>
-        <StyledContainer> 
-          {story()}
-        </StyledContainer>
-      </Router>
-    </ThemeProvider>
+    <Provider {...rootStore}>
+      <ThemeProvider theme={theme}>
+        <Router history={createBrowserHistory()}>
+          <StyledContainer> 
+            {story()}
+          </StyledContainer>
+        </Router>
+      </ThemeProvider>
+    </Provider>
   )
 })
+
+addDecorator(withKnobs)
 
 configure(loadStories, module)
