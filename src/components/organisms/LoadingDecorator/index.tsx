f30de1b3d1a77a17b393f75c2850587351a6bcb4
jsh901220 <joe1220@daum.net>
2019-07-12 17:31:54 +0900

    [dev: component] -[add] Loading Decorator 컴포넌트 추가

---
+++ src/components/organisms/LoadingDecorator/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
+import { ChasingDots } from 'styled-spinkit'
+
+interface Ioption {
+  fetchData: Function
+}
+
+export default (option: Ioption): any => {
+  return Component => {
+    @observer
+    class LoadingDecorator extends React.Component {
+      @observable isFetching: boolean
+      constructor(props) {
+        super(props)
+
+        this.isFetching = false
+      }
+
+      async componentDidMount() {
+        await option.fetchData()
+          .then(() => console.log('done!'))
+        this.isFetching = true
+      }
+
+      render() {
+        return (
+          <React.Fragment>
+            {!this.isFetching && <ChasingDots />}
+            {this.isFetching && <Component {...this.props} />}
+          </React.Fragment>
+        )
+      }
+    }
+    return LoadingDecorator
+  }
+}
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,4 +1,5 @@
 export { default as NavBar } from './NavBar'
+export { default as LoadingDecorator } from './LoadingDecorator'
 
 // Forms 
 export { default as LoginForm } from './Forms/LoginForm'
