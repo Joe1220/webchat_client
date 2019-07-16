5e08f8d7ef8607e196fc40f353ef287dd5baa40d
jsh901220 <joe1220@daum.net>
2019-07-16 20:44:49 +0900

    [dev: nav] - [add] query 를 위한 함수 추가(query-string 설치)

--- src/stores/NavStore.ts
+++ src/stores/NavStore.ts
@@ -1,10 +1,24 @@
+import { computed, action } from 'mobx'
+import queryString from 'query-string'
 import { RouteComponentProps } from 'react-router-dom'
  
 class NavStore {
   routing: RouteComponentProps
   constructor(...args: any) {
-
     this.routing = args[0].routing
+  }
+
+  @computed
+  get _params() {
+    return queryString.parse(this.routing.location.search) 
+  }
+
+  @action
+  goDetailPage(pathname, id) {
+    this.routing.history.push({
+      pathname,
+      search: `?id=${id}`
+    })
   }
 }
 
