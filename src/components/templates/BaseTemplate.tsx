e0441f4c88bbbcc898bc01951b1e09b0e7543864
jsh901220 <joe1220@daum.net>
2019-07-18 20:32:15 +0900

    [dev: component] - [fix] BaseTemplate의 content 스타일 수정

--- src/components/templates/BaseTemplate.tsx
+++ src/components/templates/BaseTemplate.tsx
@@ -5,25 +5,32 @@ import { observer } from 'mobx-react'
 import { NavBar } from 'components/organisms'
 
 const StyledBaseTemplate = styled('div')`
+  height: 100vh;
   display: flex;
   flex-direction: column;
   padding-top: 2rem;
-  min-height: 100vh;
   box-sizing: border-box;
 `
 
-const StyledContent = styled('div')`
-  width: calc(100% - 40px);
+const StyledContent = styled('section')`
+  flex: 1 1 auto;
   box-sizing: border-box;
   padding: ${props => props.theme.spaces.lg};
   min-width: 920px;
 `
 
-export default observer(({ nav, children, ...props }) => (
+const StyledStaticComponent = styled('div')`
+  box-sizing: border-box;
+  padding: ${props => props.theme.spaces.lg};
+`
+
+export default observer(({ nav, HeadComponent, children, Footer, ...props }) => (
   <StyledBaseTemplate {...props}>
     {nav && <NavBar />}
+    {HeadComponent && <StyledStaticComponent><HeadComponent /></StyledStaticComponent>}
     <StyledContent {...props}>
       {children}
     </StyledContent>
+    {Footer && <StyledStaticComponent><Footer /></StyledStaticComponent>}
   </StyledBaseTemplate>
 ))
