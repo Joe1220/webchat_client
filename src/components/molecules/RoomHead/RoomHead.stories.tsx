0a1717e413f51ebc9e3f2fe06fa853b339fc02f3
jsh901220 <joe1220@daum.net>
2019-07-18 17:33:56 +0900

    [bug: import] - [fix] RoomHead, Messagefield import 누락

--- src/components/molecules/RoomHead/RoomHead.stories.tsx
+++ src/components/molecules/RoomHead/RoomHead.stories.tsx
@@ -1,1 +1,16 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import RoomHead from './'
+import rootStore from 'stores/RootStore'
+
+const stories = storiesOf('Components/Molecules', module)
+
+stories.add('RoomHead', () => (
+  <React.Fragment>
+      <h2>Room 페이지 상단(title은 mobx store에서 관리하기 때문에 지금은 빈칸)</h2>
+      <RoomHead roomStore={rootStore.roomStore} />
+  </React.Fragment>
+))
+
 export default undefined
