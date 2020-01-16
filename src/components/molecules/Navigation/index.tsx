11d27d00d3dc48a18e34e909d377af6509bc2e0c
joe <jsh901220@gmail.com>
2020-01-17 07:23:09 +0900

    [fix: url] - resume url변경

--- src/components/molecules/Navigation/index.tsx
+++ src/components/molecules/Navigation/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { observer } from 'mobx-react'
+import React from "react"
+import { observer } from "mobx-react"
 
-import { Nav, Link, A } from 'components/atoms'
+import { Nav, Link, A } from "components/atoms"
 
 const Navigation: React.FC = observer(() => {
   return (
     <Nav>
-      <li><Link to='/'>Lobby</Link></li>
-      <li><A href='http://jshresume.co.nf/'>About</A></li>
-      <li><Link to='/how-many'>HOW MANY</Link></li>
+      <li>
+        <Link to="/">Lobby</Link>
+      </li>
+      <li>
+        <A href="https://jshresume.joe12201.now.sh/">About</A>
+      </li>
+      <li>
+        <Link to="/how-many">HOW MANY</Link>
+      </li>
     </Nav>
   )
 })
